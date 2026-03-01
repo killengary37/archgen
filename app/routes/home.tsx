c7f521f14e47a583af80a1170b82775a7594f9af
Gary Killen <killengary37@gmail.com>
2026-02-28 20:33:17 -0800

    fix coderabbit suggested bugs

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -125,7 +125,7 @@ export default function Home() {
                                     <div className="meta">
                                         <Clock size={12} />
                                         <span>{new Date(timestamp).toLocaleDateString()}</span>
-                                        <span>By JS Mastery</span>
+                                        <span>Gary Killen</span>
                                     </div>
                                 </div>
                                 <div className="arrow">
